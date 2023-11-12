fb8928673ae409f2627892cfaa7d362159b3e2e5
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-11-12 19:11:00 +0100

    refactor: extract level state from game state
    As a prepartion for hazard implementation

--- src/level/mod.rs
+++ src/level/mod.rs
@@ -1,23 +1,141 @@
 use alloc::format;
 use alloc::vec::Vec;
+use core::time::Duration;
 
 use anyhow::anyhow;
+use playdate_sys::println;
 
-use crankit_graphics::image::Image;
-use crankit_graphics::LoadError;
+use collision::Aabb;
+use crankit_graphics::{image::Image, LoadError};
+use crankit_input::ButtonState;
 use grid::Grid;
 
-use crate::{Vector, TILE_SIZE};
+use crate::{lift::Lift, player::Player, water::Water, Images, Vector, TILE_SIZE};
 
-pub struct Level {}
+const PENETRATION_RESOLUTION_MAX_ITER: u32 = 10;
 
-impl From<LevelDef> for Level {
-    fn from(value: LevelDef) -> Self {
-        todo!()
+pub struct Level {
+    definition: Definition,
+    player: Player,
+    water: Water,
+    lifts: Vec<Lift>,
+    active_lift: Option<usize>,
+}
+
+impl Level {
+    pub fn update(&mut self, delta_time: Duration, buttons: ButtonState, crank_change: f32) {
+        self.player.handle_input(buttons);
+        self.player.update(delta_time);
+        let player_collision_box = self.player.collision_box();
+        self.resolve_collisions();
+        if let Some(lift) = self.active_lift.map(|i| &mut self.lifts[i]) {
+            if player_collision_box.collides(lift.interaction_box()) {
+                lift.update(crank_change, &mut self.player);
+            } else {
+                self.active_lift = None;
+            }
+        } else if let Some(index) = (0..self.lifts.len()).find(|i| {
+            self.lifts[*i]
+                .interaction_box()
+                .collides(player_collision_box)
+        }) {
+            self.active_lift = Some(index);
+        }
+        self.water.update(delta_time);
+    }
+
+    pub fn draw(&self, images: &Images) {
+        self.definition
+            .background
+            .iter()
+            .for_each(|i| i.draw([0, 0]));
+        self.player.draw(&images.player_images);
+        self.lifts.iter().for_each(|l| l.draw(&images.lift_image));
+        self.definition
+            .foreground
+            .iter()
+            .for_each(|i| i.draw([0, 0]));
+        self.water.draw(&images.water_images);
+    }
+
+    fn resolve_collisions(&mut self) {
+        let mut iter = 0;
+        while let Some(penetration) = self.collides_against_terrain() {
+            iter += 1;
+            if iter > PENETRATION_RESOLUTION_MAX_ITER {
+                println!(
+                    "Exhausted number of iteration for inter-penetration resolution ({})",
+                    PENETRATION_RESOLUTION_MAX_ITER
+                );
+                return;
+            }
+            self.player.move_by(penetration);
+            if penetration.y < 0. {
+                self.player.on_floor_hit();
+            } else if penetration.y > 0. {
+                self.player.on_roof_hit();
+            }
+        }
+    }
+
+    fn collides_against_terrain(&self) -> Option<Vector> {
+        let player_collision_box = self.player.collision_box();
+        let terrain = coords(player_collision_box)
+            .filter(|c| matches!(self.definition.grid.get(*c), Some(Cell::Terrain)))
+            .map(|[x, y]| {
+                Aabb::from_min_max([x as f32, y as f32], [(x + 1) as f32, (y + 1) as f32])
+            });
+        let lifts = self.lifts.iter().map(|l| l.collision_box());
+        player_collision_box
+            .max_penetration(terrain.chain(lifts))
+            .map(Into::into)
+    }
+
+    fn collides_against_hazard(&self) -> bool {
+        if self.player.position().y > self.water.position() {
+            return true;
+        }
+        let player_collision_box = self.player.collision_box();
+        let hazards = coords(player_collision_box)
+            .filter(|c| matches!(self.definition.grid.get(*c), Some(Cell::Hazard)))
+            .map(|[x, y]| {
+                Aabb::from_min_max(
+                    [x as f32 + 0.1, y as f32 + 0.1],
+                    [x as f32 + 0.8, x as f32 + 0.8],
+                )
+            });
+        player_collision_box.collides_any(hazards)
     }
 }
 
-pub struct LevelDef {
+impl From<Definition> for Level {
+    fn from(definition: Definition) -> Self {
+        let player = Player::new(definition.player_start);
+        let lifts = definition
+            .lifts
+            .iter()
+            .copied()
+            .map(|(base, height)| Lift::new(base, height))
+            .collect();
+        Self {
+            definition,
+            player,
+            lifts,
+            active_lift: None,
+            water: Water::new(),
+        }
+    }
+}
+
+fn coords(bounding_box: Aabb) -> impl Iterator<Item = [usize; 2]> {
+    let [min_x, max_x] = bounding_box.x.into();
+    let [min_y, max_y] = bounding_box.y.into();
+    ((libm::floorf(min_x) as usize)..=(libm::ceilf(max_x) as usize)).flat_map(move |x| {
+        ((libm::floorf(min_y) as usize)..=(libm::ceilf(max_y) as usize)).map(move |y| [x, y])
+    })
+}
+
+pub struct Definition {
     pub background: [Image; 2],
     pub foreground: [Image; 2],
     pub player_start: Vector,
@@ -25,7 +143,7 @@ pub struct LevelDef {
     pub lifts: Vec<(Vector, f32)>,
 }
 
-impl LevelDef {
+impl Definition {
     pub fn load(num: usize) -> anyhow::Result<Self> {
         let (background, foreground) =
             Self::load_images(num).map_err(|err| anyhow!("failed to load level images: {err}"))?;
--- src/lib.rs
+++ src/lib.rs
@@ -6,19 +6,15 @@ use alloc::vec::Vec;
 use core::time::Duration;
 
 use anyhow::anyhow;
-use libm::{ceilf, floorf};
 use playdate_sys::println;
 
-use collision::Aabb;
 use crankit_game_loop::game_loop;
 use crankit_graphics::{image::Image, Color};
 use crankit_input::{button_state, crank_change};
 use crankit_time::reset_elapsed_time;
-use level::{Cell, LevelDef};
-use player::Player;
+use level::Definition;
 
-use crate::lift::Lift;
-use crate::water::Water;
+use crate::level::Level;
 
 mod animation;
 mod level;
@@ -32,8 +28,6 @@ type IVector = math2d::Vector<i32>;
 const TILE_SIZE: f32 = 16.0;
 const SCREEN_WIDTH: i32 = 400;
 const SCREEN_HEIGHT: i32 = 240;
-
-const PENETRATION_RESOLUTION_MAX_ITER: u32 = 10;
 
 struct Images {
     player_images: player::Images,
@@ -59,11 +53,7 @@ impl Images {
 
 struct Game {
     images: Images,
-    level: LevelDef,
-    player: Player,
-    water: Water,
-    lifts: Vec<Lift>,
-    active_lift: Option<usize>,
+    level: Level,
     #[cfg(feature = "draw-fps")]
     frame_durations: Vec<Duration>,
 }
@@ -73,22 +63,11 @@ const FRAME_WINDOW: usize = 30;
 
 impl crankit_game_loop::Game for Game {
     fn new() -> Self {
-        let level = LevelDef::load(0).unwrap();
+        let level = Definition::load(0).unwrap().into();
         let images = Images::load().unwrap();
-        let player = Player::new(level.player_start);
-        let lifts = level
-            .lifts
-            .iter()
-            .copied()
-            .map(|(base, height)| Lift::new(base, height))
-            .collect();
         Self {
             images,
             level,
-            player,
-            lifts,
-            active_lift: None,
-            water: Water::new(),
             #[cfg(feature = "draw-fps")]
             frame_durations: Vec::with_capacity(FRAME_WINDOW),
         }
@@ -112,95 +91,16 @@ impl crankit_game_loop::Game for Game {
 impl Game {
     fn update(&mut self, delta_time: Duration) {
         let buttons = button_state();
-        self.player.handle_input(buttons);
-        self.player.update(delta_time);
-        let player_collision_box = self.player.collision_box();
-        self.resolve_collisions();
-        if let Some(lift) = self.active_lift.map(|i| &mut self.lifts[i]) {
-            if player_collision_box.collides(lift.interaction_box()) {
-                lift.update(crank_change(), &mut self.player);
-            } else {
-                self.active_lift = None;
-            }
-        } else if let Some(index) = (0..self.lifts.len()).find(|i| {
-            self.lifts[*i]
-                .interaction_box()
-                .collides(player_collision_box)
-        }) {
-            self.active_lift = Some(index);
-        }
-        self.water.update(delta_time);
+        let crank_change = crank_change();
+        self.level.update(delta_time, buttons, crank_change);
     }
 
     fn draw(&mut self) {
         crankit_graphics::clear(Color::black());
-        self.level.background.iter().for_each(|i| i.draw([0, 0]));
-        self.player.draw(&self.images.player_images);
-        self.lifts
-            .iter()
-            .for_each(|l| l.draw(&self.images.lift_image));
-        self.level.foreground.iter().for_each(|i| i.draw([0, 0]));
-        self.water.draw(&self.images.water_images);
+        self.level.draw(&self.images);
         #[cfg(feature = "draw-fps")]
         crankit_graphics::draw_fps([0, 0]);
     }
-
-    fn resolve_collisions(&mut self) {
-        let mut iter = 0;
-        while let Some(penetration) = self.collides_against_terrain() {
-            iter += 1;
-            if iter > PENETRATION_RESOLUTION_MAX_ITER {
-                println!(
-                    "Exhausted number of iteration for inter-penetration resolution ({})",
-                    PENETRATION_RESOLUTION_MAX_ITER
-                );
-                return;
-            }
-            self.player.move_by(penetration);
-            if penetration.y < 0. {
-                self.player.on_floor_hit();
-            } else if penetration.y > 0. {
-                self.player.on_roof_hit();
-            }
-        }
-    }
-
-    fn collides_against_terrain(&self) -> Option<Vector> {
-        let player_collision_box = self.player.collision_box();
-        let terrain = coords(player_collision_box)
-            .filter(|c| matches!(self.level.grid.get(*c), Some(Cell::Terrain)))
-            .map(|[x, y]| {
-                Aabb::from_min_max([x as f32, y as f32], [(x + 1) as f32, (y + 1) as f32])
-            });
-        let lifts = self.lifts.iter().map(|l| l.collision_box());
-        player_collision_box
-            .max_penetration(terrain.chain(lifts))
-            .map(Into::into)
-    }
-
-    fn collides_against_hazard(&self) -> bool {
-        if self.player.position().y > self.water.position() {
-            return true;
-        }
-        let player_collision_box = self.player.collision_box();
-        let hazards = coords(player_collision_box)
-            .filter(|c| matches!(self.level.grid.get(*c), Some(Cell::Hazard)))
-            .map(|[x, y]| {
-                Aabb::from_min_max(
-                    [x as f32 + 0.1, y as f32 + 0.1],
-                    [x as f32 + 0.8, x as f32 + 0.8],
-                )
-            });
-        player_collision_box.collides_any(hazards)
-    }
-}
-
-fn coords(bounding_box: Aabb) -> impl Iterator<Item = [usize; 2]> {
-    let [min_x, max_x] = bounding_box.x.into();
-    let [min_y, max_y] = bounding_box.y.into();
-    ((floorf(min_x) as usize)..=(ceilf(max_x) as usize)).flat_map(move |x| {
-        ((floorf(min_y) as usize)..=(ceilf(max_y) as usize)).map(move |y| [x, y])
-    })
 }
 
 game_loop!(Game);
